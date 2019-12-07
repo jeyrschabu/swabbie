547e70d959ec6093d1bb7f2379806b7c0b1d5daf
jeyrs <jeyrschabu@gmail.com>
2019-12-07 00:16:51 -0800

    feat(rules): Adding some basic rules
    - added some smaller server group rules
    - added a general age rule

---
+++ swabbie-core/src/main/kotlin/com/netflix/spinnaker/swabbie/rules/AgeRule.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2019 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License")
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.netflix.spinnaker.swabbie.rules
+
+import com.netflix.spinnaker.config.ResourceTypeConfiguration.RuleDefinition
+import com.netflix.spinnaker.swabbie.model.Resource
+import com.netflix.spinnaker.swabbie.model.Result
+import com.netflix.spinnaker.swabbie.model.Rule
+import com.netflix.spinnaker.swabbie.model.Summary
+import java.time.Clock
+
+class AgeRule(
+  private val clock: Clock
+) : Rule {
+  override fun <T : Resource> applicableForType(clazz: Class<T>): Boolean = true
+  override fun <T : Resource> apply(resource: T): Result {
+    return apply(resource, null)
+  }
+
+  override fun <T : Resource> apply(resource: T, ruleDefinition: RuleDefinition?): Result {
+    val age = resource.age(clock).toDays()
+    val moreThanDays = ruleDefinition?.parameters?.get("moreThanDays") as? Long
+    if (moreThanDays == null || age < moreThanDays) {
+      return Result(null)
+    }
+
+    return Result(
+      Summary(
+        description = "Resource ${resource.resourceId} is older than $moreThanDays days.",
+        ruleName = name()
+      )
+    )
+  }
+}
